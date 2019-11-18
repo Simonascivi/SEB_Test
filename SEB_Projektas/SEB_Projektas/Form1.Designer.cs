ab0ebb1e71248f491d8e752ee95950b112c9167c
Simonascivi <43064529+Simonascivi@users.noreply.github.com>
2019-11-18 10:32:35 -0800

    Add files via upload

---
+++ SEB_Projektas/SEB_Projektas/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SEB_Projektas
+{
+    partial class IBANTikrinimas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.variantas1 = new System.Windows.Forms.Label();
+            this.iveskiteLabel = new System.Windows.Forms.Label();
+            this.inputIBAN = new System.Windows.Forms.TextBox();
+            this.ivestiRankaButton = new System.Windows.Forms.Button();
+            this.IBANCheckList = new System.Windows.Forms.CheckedListBox();
+            this.variantas2 = new System.Windows.Forms.Label();
+            this.PasirinktiButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.isvalytiButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // variantas1
+            // 
+            this.variantas1.AutoSize = true;
+            this.variantas1.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.variantas1.Location = new System.Drawing.Point(75, 9);
+            this.variantas1.Name = "variantas1";
+            this.variantas1.Size = new System.Drawing.Size(257, 22);
+            this.variantas1.TabIndex = 0;
+            this.variantas1.Text = "Interaktyvusis IBAN tikrinimas";
+            // 
+            // iveskiteLabel
+            // 
+            this.iveskiteLabel.AutoSize = true;
+            this.iveskiteLabel.Location = new System.Drawing.Point(18, 51);
+            this.iveskiteLabel.Name = "iveskiteLabel";
+            this.iveskiteLabel.Size = new System.Drawing.Size(112, 13);
+            this.iveskiteLabel.TabIndex = 1;
+            this.iveskiteLabel.Text = "Įveskite IBAN numerį: ";
+            // 
+            // inputIBAN
+            // 
+            this.inputIBAN.Location = new System.Drawing.Point(136, 48);
+            this.inputIBAN.Name = "inputIBAN";
+            this.inputIBAN.Size = new System.Drawing.Size(230, 20);
+            this.inputIBAN.TabIndex = 2;
+            // 
+            // ivestiRankaButton
+            // 
+            this.ivestiRankaButton.Location = new System.Drawing.Point(21, 87);
+            this.ivestiRankaButton.Name = "ivestiRankaButton";
+            this.ivestiRankaButton.Size = new System.Drawing.Size(345, 23);
+            this.ivestiRankaButton.TabIndex = 3;
+            this.ivestiRankaButton.Text = "Tikrinti";
+            this.ivestiRankaButton.UseVisualStyleBackColor = true;
+            this.ivestiRankaButton.Click += new System.EventHandler(this.ivestiRankaButton_Click);
+            // 
+            // IBANCheckList
+            // 
+            this.IBANCheckList.FormattingEnabled = true;
+            this.IBANCheckList.Location = new System.Drawing.Point(21, 224);
+            this.IBANCheckList.Name = "IBANCheckList";
+            this.IBANCheckList.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.IBANCheckList.Size = new System.Drawing.Size(345, 184);
+            this.IBANCheckList.TabIndex = 5;
+            // 
+            // variantas2
+            // 
+            this.variantas2.AutoSize = true;
+            this.variantas2.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.variantas2.Location = new System.Drawing.Point(75, 127);
+            this.variantas2.Name = "variantas2";
+            this.variantas2.Size = new System.Drawing.Size(247, 22);
+            this.variantas2.TabIndex = 6;
+            this.variantas2.Text = "IBAN Kodų tikrinimas iš failo";
+            // 
+            // PasirinktiButton
+            // 
+            this.PasirinktiButton.Location = new System.Drawing.Point(21, 163);
+            this.PasirinktiButton.Name = "PasirinktiButton";
+            this.PasirinktiButton.Size = new System.Drawing.Size(345, 23);
+            this.PasirinktiButton.TabIndex = 7;
+            this.PasirinktiButton.Text = "Pasirinkti įvesties failą";
+            this.PasirinktiButton.UseVisualStyleBackColor = true;
+            this.PasirinktiButton.Click += new System.EventHandler(this.PasirinktiButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IBAN kodų tikrinimo istorija";
+            // 
+            // isvalytiButton
+            // 
+            this.isvalytiButton.Location = new System.Drawing.Point(151, 414);
+            this.isvalytiButton.Name = "isvalytiButton";
+            this.isvalytiButton.Size = new System.Drawing.Size(99, 23);
+            this.isvalytiButton.TabIndex = 9;
+            this.isvalytiButton.Text = "Išvalyti istoriją";
+            this.isvalytiButton.UseVisualStyleBackColor = true;
+            this.isvalytiButton.Click += new System.EventHandler(this.isvalytiButton_Click);
+            // 
+            // IBANTikrinimas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 443);
+            this.Controls.Add(this.isvalytiButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasirinktiButton);
+            this.Controls.Add(this.variantas2);
+            this.Controls.Add(this.IBANCheckList);
+            this.Controls.Add(this.ivestiRankaButton);
+            this.Controls.Add(this.inputIBAN);
+            this.Controls.Add(this.iveskiteLabel);
+            this.Controls.Add(this.variantas1);
+            this.Name = "IBANTikrinimas";
+            this.Text = "IBAN Tikrinimo Programa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label variantas1;
+        private System.Windows.Forms.Label iveskiteLabel;
+        private System.Windows.Forms.TextBox inputIBAN;
+        private System.Windows.Forms.Button ivestiRankaButton;
+        private System.Windows.Forms.CheckedListBox IBANCheckList;
+        private System.Windows.Forms.Label variantas2;
+        private System.Windows.Forms.Button PasirinktiButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button isvalytiButton;
+    }
+}
+
---
+++ SEB_Projektas/SEB_Projektas/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEB_Projektas
+{
+    public partial class IBANTikrinimas : Form
+    {
+         LinkedList<string> IBANList=new LinkedList<string>();
+         LinkedList<string> IBANShortList = new LinkedList<string>();
+
+        static void Klaida()
+        {
+            MessageBox.Show("Dėmesio! Programa susidūrė su problema.","Klaida");
+        }
+
+        public IBANTikrinimas()
+        {
+            InitializeComponent();
+        }
+
+        private void ivestiRankaButton_Click(object sender, EventArgs e)
+        {
+            string IBAN = inputIBAN.Text;
+            if (IBAN == null)
+            {
+                Klaida();
+            }
+
+            else
+            {
+                bool tinkamas = TikrintiIBAN(IBAN);
+                if (tinkamas)
+                {
+                    MessageBox.Show("IBAN numeris atitinka reikalavimus ir yra unikalus","IBAN tinkamas");
+                }
+            }
+
+        }
+        bool TikrintiIBAN(string IBAN)
+        {
+            //Struktūros tikrinimas
+            if (IBAN[0] != 'L' || IBAN[1] != 'T'|| IBAN.Length != 18|| IBANShortList.Find(IBAN.Substring(4)) != null)
+            {
+                IBANList.AddLast(IBAN);
+                IBANCheckList.Items.Add(IBAN, false);
+                return false;
+            }
+            IBANList.AddLast(IBAN);
+            IBANShortList.AddLast(IBAN.Substring(4));
+            IBANCheckList.Items.Add(IBAN, true);
+            return true;
+        }
+
+        private void PasirinktiButton_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                string fileToOpen = FD.FileName;
+                StreamReader reader = new StreamReader(fileToOpen);
+                StreamWriter writer = new StreamWriter(Path.ChangeExtension(fileToOpen, ".out"));
+                FailoTikrinimas(reader,writer);
+            }
+        }
+        void FailoTikrinimas(StreamReader reader,StreamWriter writer)
+        {
+            int blogi = 0;
+            int geri = 0;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line != "")
+                {
+                    if (TikrintiIBAN(line))
+                    {
+                        writer.WriteLine(line + ";" + "true");
+                        writer.Flush();
+                        geri++;
+                    }
+                    else
+                    {
+                        writer.WriteLine(line + ";" + "false");
+                        writer.Flush();
+                        blogi++;
+                    }
+                }
+            }
+            writer.Close();
+            MessageBox.Show("Failo tikrinimas baigtas \n Tinkamų IBAN: "+geri+"\n Netinkamų IBAN: "+blogi);
+        }
+
+        private void isvalytiButton_Click(object sender, EventArgs e)
+        {
+            IBANShortList.Clear();
+            IBANList.Clear();
+            IBANCheckList.Items.Clear();
+        }
+    }
+}
